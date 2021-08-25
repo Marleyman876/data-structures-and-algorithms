cc12b4fde1bb55d1b6a5b1ba892dbd118664ae2d
Marleyman876 <grant_garfield@yahoo.com>
2021-08-25 18:11:02 +0200

    after hours of reading and searching think i got it figured out

--- python/code_challenges/insertions_sort/insert.py
+++ python/code_challenges/insertions_sort/insert.py
@@ -1,12 +1,7 @@
- InsertionSort(int[] arr)
-
-    FOR i = 1 to arr.length
-
-      int j <-- i - 1
-      int temp <-- arr[i]
-
-      WHILE j >= 0 AND temp < arr[j]
-        arr[j + 1] <-- arr[j]
-        j <-- j - 1
-
-      arr[j + 1] <-- temp
+def insertion_sort(list):
+  for index, value in enumerate(list):
+    j = index - 1
+    while j >= 0 and value < list[j]:
+      list[j+1] = list[j]
+      j -= 1
+    list[j+1] = value
